aceed9579e18021ee8adffd3fd6df29a9b57261d
Sumon Chhetry <sumonchhetry1@gmail.com>
2022-04-15 01:57:34 +0530

    first commint

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for a month"
+    return HttpResponse()
